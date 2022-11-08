b07e08dd4162e8419075c2e0ab10d0fd2ce7b09f
John_the_FlashInTheNight <thecreepystyle@gmail.com>
2022-11-09 00:28:01 +0200

    Сделал две функий из задания 2.15

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-let a = 10;
-let b = 3;
+let getRandomNumber = (min = 1, max = 10) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+};
+
+let checkLengthLine = (line, maxLength) => {
+  return (line.length <= maxLength) ? true : false
+};
+
+// getRandomNumber(1, 10);
+// checkLengthLine('test', 10);
