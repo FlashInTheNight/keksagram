75538609edfc11a002a8a3f5106756e19ef1f8c9
John_the_FlashInTheNight <thecreepystyle@gmail.com>
2022-11-18 21:40:06 +0200

    7.11 Выполнил полностью пункты 1 и 2 ТЗ

--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,32 @@ const modalWindow = document.querySelector('.big-picture');
 const bigPicture = modalWindow.querySelector('.big-picture__img').querySelector('img');
 const likes = modalWindow.querySelector('.likes-count');
 const comments = modalWindow.querySelector('.comments-count');
+const commentsList = modalWindow.querySelector('.social__comments');
+const caption = modalWindow.querySelector('.social__caption');
+
+
+const addCommentsItem = function (commentElement) {
+  commentsList.innerHTML = '';
+  commentElement.comments.forEach((element) => {
+    commentsList.insertAdjacentHTML('beforeEnd', `
+    <li class="social__comment">
+      <img
+          class="social__picture"
+          src="${element.avatar}"
+          alt="${element.name}"
+          width="35" height="35">
+      <p class="social__text">${element.message}</p>
+    </li>
+    `);
+  });
+};
 
 const changeData = function (data) {
   bigPicture.src = data.url;
   likes.textContent = data.likes;
   comments.textContent = data.comments.length;
+  addCommentsItem(data);
+  caption.textContent = data.description;
 };
 
 const CreateListener = function (arrPics, element) {
