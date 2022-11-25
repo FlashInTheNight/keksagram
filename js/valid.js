e0181a74d1d88b2c3245625959cb4e24f2aac008
John_the_FlashInTheNight <thecreepystyle@gmail.com>
2022-11-25 21:35:51 +0200

    Правда или действие (часть 1) / Сделал валидацию хеш-тегов

--- js/valid.js
+++ js/valid.js
@@ -1,18 +1,45 @@
 const uploadForm = document.querySelector('.img-upload__form');
-
+const uploadTextHashTag = uploadForm.querySelector('.text__hashtags');
 
 const pristine = new Pristine(uploadForm, {
-  classTo: 'text__hashtags',
-  errorClass: 'text__hashtags--invalid',
-  successClass: 'text__hashtags--valid',
-  errorTextParent: 'text__hashtags',
-  errorTextTag: 'span',
-  errorTextClass: 'text__error'
+  classTo: 'img-upload__element', // Элемент, на который будут добавляться классы
+  errorTextParent: 'img-upload__element', // Элемент, куда будет выводиться текст с ошибкой
+  errorTextClass: 'img-upload_error' // Класс для элемента с текстом ошибки
 });
 
-pristine.addValidator(uploadForm.querySelector('.text__hashtags'));
 
-uploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
+const validateTags = (value) => {
+  const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+  let valid = false;
+  const arrValue = value.split(' ');
+
+  function checkRegExp (e) {
+    valid = re.test(e);
+  }
+
+  arrValue.forEach((element) => checkRegExp(element));
+
+  for (let i = 1; i < arrValue.length; i++) {
+    if (arrValue[i-1] === arrValue[i]) {
+      valid = false;
+    }
+  }
+
+  if (arrValue.length > 5) {
+    valid = false;
+  }
+
+  return valid;
+};
+
+pristine.addValidator(
+  uploadTextHashTag,
+  validateTags,
+  'Неправильно заполнены хэштеги'
+);
+
+
+// uploadForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   pristine.validate();
+// });
